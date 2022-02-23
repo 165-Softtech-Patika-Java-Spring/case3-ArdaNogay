1ee381d2f28923bbac3109e382d3bd980d2b2883
Ceyhan Arda Nogay <ardanogay@hotmail.com>
2022-02-23 17:11:06 +0300

    First commit

---
+++ src/main/java/softtech/softtechspringboot/SofttechSpringBootApplication.java
@@ -0,0 +1,13 @@
+package softtech.softtechspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofttechSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofttechSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/softtech/softtechspringboot/SofttechSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package softtech.softtechspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofttechSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
